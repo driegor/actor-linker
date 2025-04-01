91e88cc48e6d33d1669d1d58156d1fb150f16777
daniel.riego <daniel.riego@adevinta.com>
2025-04-01 19:49:39 +0200

    Add initial structure

---
+++ src/main/kotlin/com/devgor/actor/linker/ActorLinkerApplication.kt
@@ -0,0 +1,11 @@
+package com.devgor.actor.linker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ActorLinkerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ActorLinkerApplication>(*args)
+}
